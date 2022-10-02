a3e83dd1b1dde1174530d00fb22ac23eab37d6ad
adivianahd <adivianahd07@gmail.com>
2022-10-02 15:26:01 -0300

    add animation shareElement iin focus screen

--- src/screens/Focus.tsx
+++ src/screens/Focus.tsx
@@ -1,8 +1,14 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import * as React from 'react';
-import {SafeAreaView, ScrollView, StatusBar, Pressable} from 'react-native';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
 import {SharedElement} from 'react-navigation-shared-element';
 import {Text, Card, Image} from '../components';
+import {Capitalize} from '../helpers/utils';
 
 type RootStackParamList = {
   Home: {};
@@ -19,25 +25,25 @@ const Focus = (props: FocusProps) => {
     <SafeAreaView>
       <StatusBar />
       <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <SharedElement id={`pokemon.${pokemon.id}`}>
-          <Card size="M" color="YELLOW">
+        <Card color="YELLOW">
+          <SharedElement id={`pokemon.${pokemon.id}.image`}>
             <Image
-              size="M"
+              size="L"
               source={{
                 uri: pokemon.sprites.front_default,
               }}
             />
-            <Text class="TITLE">{pokemon.name}</Text>
-            <Pressable
-              onPress={() => navigation.push('Detail', {pokemon})}
-              style={({pressed}) => ({
-                width: 50,
-                height: 50,
-                backgroundColor: pressed ? '#fcc' : '#ccc',
-              })}
-            />
-          </Card>
-        </SharedElement>
+          </SharedElement>
+          <SharedElement id={`pokemon.${pokemon.id}.name`}>
+            <Text class="TITLE">{Capitalize(pokemon.name)}</Text>
+          </SharedElement>
+          <SharedElement id={`pokemon.${pokemon.id}.details`}>
+            <TouchableOpacity
+              onPress={() => navigation.push('Detail', {pokemon})}>
+              <Text class="COMMON">More details...</Text>
+            </TouchableOpacity>
+          </SharedElement>
+        </Card>
       </ScrollView>
     </SafeAreaView>
   );
